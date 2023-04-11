4f625305560f04199b8fe46454dd2376d42aaaea
Smachy <pro.diegocdossantos@gmail.com>
2023-04-11 15:51:32 +0200

    init thread

--- client.py
+++ client.py
@@ -1,10 +1,12 @@
 import socket
+import threading
 
 class Client:
 
     def __init__(self, server_host, server_port):
         self.__server_host = server_host
         self.__server_port = server_port
+        self.connect()
     
     def get_server_host(self):
         return self.__server_host
@@ -12,6 +14,11 @@ class Client:
     def get_server_port(self):
         return self.__server_port
     
+    def connect(self):
+        sckt = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #socket.AF_INET = IPV4 address, SOCK_STREAM = use TCP protocol
+        sckt.connect((self.__server_host,self.__server_port)) #Connect socket to server
+    
+
     def send_message(self,message):
         sckt = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #socket.AF_INET = IPV4 address, SOCK_STREAM = use TCP protocol
         sckt.connect((self.__server_host,self.__server_port)) #Connect socket to server
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from client import Client
+import threading
 
 HOST = '10.57.33.239'  #IP address server machine
 PORT = 3042
 
 client = Client(HOST,PORT)
 
+message_thread = threading.Thread(client.send_message(), args=("test"))
