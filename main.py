a6b5e5df61411ba98fd5b67c89a056d8aa3e3554
Val <pro.mvaldo@gmail.com>
2023-04-11 15:13:34 +0200

    message text test

--- main.py
+++ main.py
@@ -4,4 +4,8 @@ HOST = '10.57.33.239'  #IP address server machine
 PORT = 3042
 
 client = Client(HOST,PORT)
+client.send_message("test")
+client.send_message("test")
+client.send_message("test")
+client.send_message("test")
 
