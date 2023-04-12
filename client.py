7d1751662b4891f96a8352120a8266bd8df3dd45
Smachy <pro.diegocdossantos@gmail.com>
2023-04-12 08:59:41 +0200

    set pseudo

--- client.py
+++ client.py
@@ -7,6 +7,7 @@ class Client:
         self.__server_host = server_host
         self.__server_port = server_port
         self.__sckt = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #socket.AF_INET = IPV4 address, SOCK_STREAM = use TCP protocol
+        self.__pseudo = self.create_pseudo()
         self.connect()
     
     def get_server_host(self):
@@ -14,6 +15,17 @@ class Client:
     
     def get_server_port(self):
         return self.__server_port
+    
+    def get_pseudo(self):
+        return self.__pseudo
+    
+    def create_pseudo(self):
+        tmp_pseudo = input("Enter pseudo : ")
+        
+        while(tmp_pseudo >20):
+            tmp_pseudo = input("Enter pseudo : ")
+        
+        self.__pseudo = tmp_pseudo
     
     def connect(self):
         self.__sckt.connect((self.__server_host,self.__server_port)) #Connect socket to server
